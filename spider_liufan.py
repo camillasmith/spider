70466116a00fe3bbcf98f1b231ec668dd7bd9df9
camilla <31853167+camillasmith@users.noreply.github.com>
2017-09-11 23:03:02 +0800

    抓取百度图片
    抓取百度图片

---
+++ spider_liufan.py
@@ -0,0 +1,26 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+import urllib.request
+import io
+import sys
+
+def getHtml(url):
+    page=urllib.request.urlopen(url)
+    html=page.read()
+    html=html.decode('utf-8')
+    print(html)
+
+def getImg(html):
+    #html=html.decode('utf-8')
+    reg=r"URL.\.jpg"
+    imgre=re.compile(reg)
+    imglist=re.findall(imgre,html)
+    print(imglist)
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030')
+html=getHtml("http://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&word=%E5%9B%BE%E7%89%87")
+img=getImg(html)
+print(img)
+
